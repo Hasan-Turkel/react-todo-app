27b66c8f5f969bfb47edd901b2f0c4d5a1e03b73
hasan <mmhotmail>
2023-07-14 17:42:56 +0300

    fix commit

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -10,7 +10,7 @@ const Todolist = ({setList, list}) => {
   }
   return (
   <main >
-    {list.map(({id, todo})=> (<div key={id}  className="list"> 
+    {list?.map(({id, todo})=> (<div key={id}  className="list"> 
   <p>
       {todo}
   </p>
